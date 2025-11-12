cb26eefd532711c29155364655b98b787b158744
Guiler <129989701+GuilhermeSerafim@users.noreply.github.com>
2025-11-12 19:30:47 +0000

    ex 3

--- lesson01/aula04.swift
+++ lesson01/aula04.swift
@@ -9,17 +9,36 @@
 // let itemASerMostrado = Int(readLine() ?? "") ?? 0
 // print("\(items[itemASerMostrado])")
 
-// 2
-let quantidadeDeItens2 = Int(readLine() ?? "") ?? 0
-var items2 = [String]()
-for _ in 0...quantidadeDeItens2 - 1 {
+// // 2
+// let quantidadeDeItens2 = Int(readLine() ?? "") ?? 0
+// var items2 = [String]()
+// for _ in 0...quantidadeDeItens2 - 1 {
+//     if let item = readLine() {
+//         items2.append(item)
+//     }
+// }
+// let itemASerMostrado2 = readLine() ?? ""
+// if items2.contains(itemASerMostrado2) {
+//     print("Total de itens: \(items2.count). O item \(itemASerMostrado2) esta no inventario")
+// } else {
+//     print("Total de itens: \(items2.count). O item \(itemASerMostrado2) nao esta no inventario")
+// }
+
+// 3
+let quantidadeDeItens3 = Int(readLine() ?? "") ?? 0
+var items3 = [String]()
+for _ in 0...quantidadeDeItens3 - 1 {
     if let item = readLine() {
-        items2.append(item)
+        let partes = i.map { String(tem.split(separator: ",")$0) }
+        items3.append(partes[0])
+        items3.append(partes[1])
     }
 }
-let itemASerMostrado2 = readLine() ?? ""
-if items2.contains(itemASerMostrado2) {
-    print("Total de itens: \(items2.count). O item \(itemASerMostrado2) esta no inventario")
+let elemento = readLine() ?? ""
+
+if(items3.contains(elemento)) {
+    let index = items3.firstIndex(of: elemento)
+    print("O feitico \(elemento) tem poder: \(items3[index! + 1])")
 } else {
-    print("Total de itens: \(items2.count). O item \(itemASerMostrado2) nao esta no inventario")
+    print("O feitico \(elemento) tem poder: 0")
 }
