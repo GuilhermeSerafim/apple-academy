a1b4adfdfb4c594eb1c7fee8b728b9c494554341
Guiler <129989701+GuilhermeSerafim@users.noreply.github.com>
2025-11-12 20:21:26 +0000

    Ex 4

--- lesson01/aula04.swift
+++ lesson01/aula04.swift
@@ -24,21 +24,57 @@
 //     print("Total de itens: \(items2.count). O item \(itemASerMostrado2) nao esta no inventario")
 // }
 
-// 3
-let quantidadeDeItens3 = Int(readLine() ?? "") ?? 0
-var items3 = [String]()
-for _ in 0...quantidadeDeItens3 - 1 {
+// // 3
+// let quantidadeDeItens3 = Int(readLine() ?? "") ?? 0
+// var items3 = [String]()
+// for _ in 0...quantidadeDeItens3 - 1 {
+//     if let item = readLine() {
+//         let partes = item.split(separator: ",").map { String($0) }
+//         items3.append(partes[0])
+//         items3.append(partes[1])
+//     }
+// }
+// let elemento = readLine() ?? ""
+
+// if(items3.contains(elemento)) {
+//     let index = items3.firstIndex(of: elemento)
+//     print("O feitico \(elemento) tem poder: \(items3[index! + 1])")
+// } else {
+//     print("O feitico \(elemento) tem poder: 0")
+// }
+
+// 4
+let quantidadeDeItens4 = Int(readLine() ?? "") ?? 0
+var missaoERecompensa = [String]()
+for _ in 0...quantidadeDeItens4 - 1 {
     if let item = readLine() {
-        let partes = i.map { String(tem.split(separator: ",")$0) }
-        items3.append(partes[0])
-        items3.append(partes[1])
+        let partes = item.split(separator: ",").map { String($0) }
+        missaoERecompensa.append(partes[0])
+        missaoERecompensa.append(partes[1])
     }
 }
-let elemento = readLine() ?? ""
+let consultasASeremFeitas = Int(readLine() ?? "") ?? 0
+var indexMissao = [Int]()
+var resultado = [String]()
+var todasAsConsultas = [String]()
 
-if(items3.contains(elemento)) {
-    let index = items3.firstIndex(of: elemento)
-    print("O feitico \(elemento) tem poder: \(items3[index! + 1])")
-} else {
-    print("O feitico \(elemento) tem poder: 0")
+for _ in 0...consultasASeremFeitas - 1 {
+    let elemento = readLine() ?? ""
+    todasAsConsultas.append(elemento)
+    if(missaoERecompensa.contains(elemento)) {
+        let index = missaoERecompensa.firstIndex(of: elemento)
+        indexMissao.append(index!)
+    } else {
+        indexMissao.append(-1)
+    }
 }
+
+for (i, idx) in indexMissao.enumerated() {
+    if idx != -1 {
+        resultado.append("A missao \(missaoERecompensa[idx]) paga: \(missaoERecompensa[idx + 1]) moedas.")
+    } else {
+        resultado.append("A missao \(todasAsConsultas[i]) nao esta disponivel.")
+    }
+}
+
+print(resultado.joined(separator: " "))
