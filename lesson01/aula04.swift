c2184547919b76b43fa6b40a1f130f81e90a2ed3
Guiler <129989701+GuilhermeSerafim@users.noreply.github.com>
2025-11-12 18:52:36 +0000

    ex2 aula4

--- lesson01/aula04.swift
+++ lesson01/aula04.swift
@@ -1,10 +1,25 @@
-// 1
-let quantidadeDeItens = Int(readLine() ?? "") ?? 0
-var items = [String]()
-for _ in 0...quantidadeDeItens - 1 {
+// // 1
+// let quantidadeDeItens = Int(readLine() ?? "") ?? 0
+// var items = [String]()
+// for _ in 0...quantidadeDeItens - 1 {
+//     if let item = readLine() {
+//         items.append(item)
+//     }
+// }
+// let itemASerMostrado = Int(readLine() ?? "") ?? 0
+// print("\(items[itemASerMostrado])")
+
+// 2
+let quantidadeDeItens2 = Int(readLine() ?? "") ?? 0
+var items2 = [String]()
+for _ in 0...quantidadeDeItens2 - 1 {
     if let item = readLine() {
-        items.append(item)
+        items2.append(item)
     }
 }
-let itemASerMostrado = Int(readLine() ?? "") ?? 0
-print("\(items[itemASerMostrado])")
+let itemASerMostrado2 = readLine() ?? ""
+if items2.contains(itemASerMostrado2) {
+    print("Total de itens: \(items2.count). O item \(itemASerMostrado2) esta no inventario")
+} else {
+    print("Total de itens: \(items2.count). O item \(itemASerMostrado2) nao esta no inventario")
+}
