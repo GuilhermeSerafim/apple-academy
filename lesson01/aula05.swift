1603771dd6ac4a51fc7181dba664a1a305bf602b
Guiler <129989701+GuilhermeSerafim@users.noreply.github.com>
2025-11-13 19:12:10 +0000

    ex 3

--- lesson01/aula05.swift
+++ lesson01/aula05.swift
@@ -8,15 +8,24 @@
 // print("Dano causado: \(calc(x, y))")
 
 // Exercício 2
+// let x = Int(readLine()!)!
+// let y = Int(readLine()!)!
+// func calc(_ a: Int, _ b: Int) -> Int {
+//     return a - b
+// }
+// let vidaRestante = calc(x, y)
+
+// if(vidaRestante <= 0) {
+//     print("Personagem derrotado")
+// } else {
+//     print("Vida restante: \(vidaRestante)")
+// }
+
+// Exercício 3
 let x = Int(readLine()!)!
 let y = Int(readLine()!)!
-func calc(_ a: Int, _ b: Int) -> Int {
-    return a - b
+let z = Int(readLine()!)!
+func calc(_ a: Int, _ b: Int, _ c: Int) -> Int {
+    (a * b) + c
 }
-let vidaRestante = calc(x, y)
-
-if(vidaRestante <= 0) {
-    print("Personagem derrotado")
-} else {
-    print("Vida restante: \(vidaRestante)")
-}
+print("Experiencia ganha: \(calc(x, y, z))")
